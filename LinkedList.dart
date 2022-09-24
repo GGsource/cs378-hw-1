234b7ebff6d5821a17a9d001f20cf0fedde13498
ggonza55 <ggonza55@uic.edu>
2022-09-23 19:07:52 -0500

    Added skeleton override methods in LinkedList

---
+++ LinkedList.dart
@@ -0,0 +1,26 @@
+import 'Collection.dart';
+
+class LinkedList<T extends Comparable> extends Collection {
+  @override
+  Collection<Comparable> add(Comparable val) {
+    // TODO: implement add
+    throw UnimplementedError();
+  }
+
+  @override
+  Collection<Comparable> copy() {
+    // TODO: implement copy
+    throw UnimplementedError();
+  }
+
+  @override
+  Comparable operator [](int index) {
+    // TODO: implement []
+    throw UnimplementedError();
+  }
+
+  @override
+  void printString() {
+    // TODO: implement printString
+  }
+}
