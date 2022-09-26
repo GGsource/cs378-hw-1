cfe676c246a8ff5f71b6dbabbfb009aa86e2f6c2
ggonza55 <ggonza55@uic.edu>
2022-09-26 18:15:42 -0500

    Added tests for equals functionality

--- test/Collection_test.dart
+++ test/Collection_test.dart
@@ -18,4 +18,43 @@ void main() {
     expect(llCap[0], "appleapple");
     expect(llCap[1], "bananabanana");
   });
+
+  test("containsC Functionality Test", () {
+    ll.add("a").add("b").add("d");
+    expect(ll.containsC("a"), true);
+    expect(ll.containsC("b"), true);
+    expect(ll.containsC("c"), false);
+    expect(ll.containsC("d"), true);
+  });
+
+  test("equals Functionality Test - Different Objects", () {
+    ll.add("abc").add("def").add("wildcard");
+    expect(ll.equals(5), false);
+  });
+  test("equals Functionality Test 2 - Different Types", () {
+    ll.add("abc").add("def").add("wildcard");
+    LinkedList<int> ll2 = new LinkedList()..add(4);
+    expect(ll.equals(ll2), false);
+  });
+  test("equals Fucntionality Test 3 - Different Sizes", () {
+    ll.add("abc").add("def").add("wildcard");
+    LinkedList<String> ll3 = new LinkedList()..add("abc");
+    expect(ll.equals(ll3), false);
+  });
+  test("equals Functionality Test 4 - Different Contents", () {
+    ll.add("abc").add("def").add("wildcard");
+    LinkedList<String> ll4 = new LinkedList()
+      ..add("abc")
+      ..add("def")
+      ..add("ghi");
+    expect(ll.equals(ll4), false);
+  });
+  test("equals Functionality Test 5 - ", () {
+    ll.add("abc").add("def").add("wildcard");
+    LinkedList<String> ll5 = new LinkedList()
+      ..add("abc")
+      ..add("def")
+      ..add("wildcard");
+    expect(ll.equals(ll5), true);
+  });
 }
