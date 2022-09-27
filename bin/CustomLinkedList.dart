7777ba420fcf326209beb5988607f2129eeab0f2
ggonza55 <ggonza55@uic.edu>
2022-09-26 19:00:15 -0500

    Added line about tests in main file

--- bin/CustomLinkedList.dart
+++ bin/CustomLinkedList.dart
@@ -2,14 +2,18 @@ import 'package:CustomLinkedList/LinkedList.dart';
 
 void main(List<String> args) {
   LinkedList<int> numberList = new LinkedList();
+
   print("Empty LinkedList:");
   numberList.printString();
+
   print("LinkedList after adding 1 item");
   numberList.add(givenVal: 200);
   numberList.printString();
+
   print("Linked list after having 4 items");
   numberList.add(givenVal: 10).add(givenVal: 20).add(givenVal: 5);
   numberList.printString();
+
   print("Now attempting to iterate through list:");
   for (var it = numberList.iterator; it.moveNext();) {
     print(it.current);
@@ -23,4 +27,7 @@ void main(List<String> args) {
   numberList.forEach((element) {
     print(element);
   });
+
+  print(
+      "Switched testing to test files in test directory. Run tests with \'dart test\'.");
 }
